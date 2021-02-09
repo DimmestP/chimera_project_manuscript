ccb92fe21b63070a504be396c27eed16bd77d693
Edward Wallace <ewjwallace@gmail.com>
2021-02-09 09:57:35 +0000

    Simplified shared_figure_formating.R to avoid extrafont and use sans
    Makes it behave better on varied systems, stopped an error on mine.

--- raw_data_analysis/code/shared_figure_formatting.R
+++ raw_data_analysis/code/shared_figure_formatting.R
@@ -1,18 +1,19 @@
 library(ggplot2)
 library(cowplot)
-library(extrafont)
+# library(extrafont)
 
-theme_set(theme_cowplot(font_size=11) %+replace% 
-            theme(plot.margin = margin(0.5, 0.5, 0.5, 0.5, "cm"),
-                  panel.border=element_rect(colour = "grey50",linetype = "solid",size=0.5),
-                  panel.grid.major = element_line(size = 0.15, linetype = 'solid',colour = "grey"),
-                  plot.title = element_text(family="FreeSans",size=18),
-                  strip.background = element_blank(),
-                  strip.text.x=element_text(family="FreeSans",size=11),
-                  axis.text=element_text(family="FreeSans", size=10),
-                  axis.title=element_text(family="FreeSans", size=15),
-                  legend.text=element_text(family="FreeSans", size=10),
-                  legend.title=element_text(family="FreeSans", size=12)))
+theme_set(
+  theme_cowplot(font_size = 12, 
+                font_family = "sans",
+                rel_small = 11/12,
+                rel_tiny = 10/12,
+                rel_large = 14/12) %+replace% 
+    theme(plot.margin = margin(0.5, 0.5, 0.5, 0.5, "cm"),
+          panel.border=element_rect(colour = "grey50",linetype = "solid",size=0.5),
+          panel.grid.major = element_line(size = 0.15, linetype = 'solid',colour = "grey"),
+          strip.background = element_blank()
+    )
+)
 
 update_geom_defaults("point", list(size = 3))
 update_geom_defaults("errorbar", list(size = 1))
