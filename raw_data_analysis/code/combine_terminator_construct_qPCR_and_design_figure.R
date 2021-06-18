1880ca8774a0b6f5b363d648a8051c484fd26bd2
Edward Wallace <ewjwallace@gmail.com>
2021-06-18 10:36:35 +0100

    upgrade modified terminator plots, fixes #126, fixes #170

--- raw_data_analysis/code/combine_terminator_construct_qPCR_and_design_figure.R
+++ raw_data_analysis/code/combine_terminator_construct_qPCR_and_design_figure.R
@@ -1,6 +1,7 @@
 library(magick)
-library(patchwork)
+library(grid)
 library(gridExtra)
+library(gtable)
 library(here)
 library(dplyr)
 library(readr)
@@ -9,10 +10,6 @@ library(stringr)
 source(here("raw_data_analysis/code/shared_figure_formatting.R"))
 
 ### TSA1 and RPS3 insertion diagram
-
-layout <- "AAA
-           BCC"
-
 
 construct_to_label_dictionary_TSA1_RPS3 = tibble(construct = c("WT","modC","modE","modD","modA","modB","mod0"), 
                                        label = c("WT", "mod_NGG", "mod_HTH", "mod_HNH", "mod_NTN", "mod_NAA", "mod_NNN"),
@@ -45,12 +42,8 @@ RPS3_deltadeltacq_plot <- ggplot(data = RPS3_deltadeltacq) +
   labs(x ="Fold change in RNA abundance relative to tRPS3 mod_NNN (log2 scale)", y = "") +
   facet_wrap(~promoter,ncol = 3) 
 
-insertion_layout <- "AC
-                     AC
-                     BC"
-
 insertion_construct_displaytable <- 
-  tableGrob(tibble("Construct" = c("mod_NNN", "mod_NAA", "mod_NTN",
+  tableGrob(tibble("Terminator" = c("mod_NNN", "mod_NAA", "mod_NTN",
                                    "mod_HNH", "mod_HTH", "mod_NGG", "WT"), 
                    "Motif 1" = c("Random", "Random", "Random", 
                                  "HWNCATTWY", "HWNCATTWY", "Random", "NA"), 
@@ -59,25 +52,45 @@ insertion_construct_displaytable <-
                    "Motif 3" = c("Random", "ATATTC", "Random", 
                                  "HWNCATTWY", "HWNCATTWY", "GTATACCTA", "NA")),
             rows = NULL,
-            theme = ttheme_default(base_size=6,
+            theme = ttheme_minimal(base_size=8,
                                    padding = unit(c(2, 2), "mm"),
                                    core=list(
                                      fg_params=list(col = c("black", "#C05558", "#5978BA", "#71519C", "#C288BB", "#7FBF74", "#878787",
                                                             "black", "black", "black", "#71519C", "#C288BB", "black", "#878787",
                                                             "black", "#C05558", "#5978BA", "black", "#C288BB", "#7FBF74", "#878787",
                                                             "black", "#C05558", "black", "#71519C", "#C288BB", "#7FBF74", "#878787")))
-            ))
-grid.arrange(insertion_construct_displaytable)
-insertion_construct_plot <- wrap_elements(full = (wrap_elements(full = (ggdraw() + 
-                                                    draw_image(image_read_svg(here("raw_data_analysis/figures/terminator_construct_designs/RPS3_TSA1_motif_mod0_construct_design.svg"))))) +
-  wrap_elements(full = insertion_construct_displaytable) + 
-  wrap_elements(full = (ggdraw() + 
-                          draw_image(image_read_svg(here("raw_data_analysis/figures/terminator_construct_designs/tRPS3-tTSA1_design.svg"), width = 520, height = 354))))+
-    plot_layout(design = insertion_layout))) /
-  RPS3_deltadeltacq_plot /
-  TSA1_deltadeltacq_plot +
-  plot_annotation(tag_levels = "A") +
-  plot_layout(heights = c(4,1,1))
+            ))  %>%
+  # the following code adds boxes around the table, row 1, and column 1
+  gtable_add_grob(
+    grobs = rectGrob(gp = gpar(fill = NA, lwd = 0.5)),
+    t = 1, b = 8, l = 1, r = 4) %>%
+  gtable_add_grob(
+    grobs = rectGrob(gp = gpar(fill = NA, lwd = 0.5)),
+    t = 1, b = 1, l = 1, r = 4) %>%
+  gtable_add_grob(
+    grobs = rectGrob(gp = gpar(fill = NA, lwd = 0.5)),
+    t = 1, b = 8, l = 1, r = 1)
+
+## print the table to see how it looks
+grid.newpage()
+grid.draw(insertion_construct_displaytable)
+
+top_row_insertion_construct_plot <- 
+  plot_grid(
+    ggdraw() + 
+      draw_image(image_read_svg(here("raw_data_analysis/figures/terminator_construct_designs/RPS3_TSA1_motif_mod0_construct_design.svg"))),
+    insertion_construct_displaytable,
+    ncol = 2)
+
+insertion_construct_plot <- 
+  plot_grid(
+    NULL, # create top margin above ggdraw; probably there is a better way
+    top_row_insertion_construct_plot,
+    RPS3_deltadeltacq_plot,
+    TSA1_deltadeltacq_plot,
+    ncol = 1,
+    rel_heights = c(0.02,1,1,1),
+    labels = c("A","", "B", "C"))
 
 ggsave(filename = here("results_chapter/figures/insertion_constructs_design_and_qpcr.png"),
        width = fig_width_2column,
@@ -104,13 +117,8 @@ PIR1_deltadeltacq_plot <- ggplot(data = PIR1_deltadeltacq)+
   labs(x ="Fold change in RNA abundance relative to tPIR1 mod_WT (log2 scale)", y = "") +
   facet_wrap(~promoter,ncol = 3)
 
-deletion_layout <- "AC
-                    BC"
-
-## Can we modularize this plot to produce the tableGrob in one place, then call it from the draw?
-
 deletion_construct_displaytable <- 
-  tableGrob(tibble("Construct" = rev(c("mod_NTNNN", "mod_ANNNN", "mod_ATNNN", "mod_ATHNH", 
+  tableGrob(tibble("Terminator" = rev(c("mod_NTNNN", "mod_ANNNN", "mod_ATNNN", "mod_ATHNH", 
                                        "mod_ATNHH", "mod_ANHHH", "mod_NTHHH", "WT")), 
                    "Motif 1" = c("ATATTC", "Random", "ATATTC", 
                                  "ATATTC", "ATATTC", "ATATTC", "ATATTC", "Random"), 
@@ -123,8 +131,8 @@ deletion_construct_displaytable <-
                    "Motif 5" = c("HWNCATTWY", "HWNCATTWY", "HWNCATTWY", 
                                  "HWNCATTWY", "HWNCATTWY", "Random", "Random", "Random")),
             rows = NULL,
-            theme = ttheme_default(
-              base_size=6,
+            theme = ttheme_minimal(
+              base_size=8,
               padding = unit(c(2, 2), "mm"),
               core=list(
                 fg_params=list(col = c("#878787", "#C15659", "#5978BA", "#4EB0B5", "#D97F1D", "#71519C", "#968BC2", "#DD76A5",
@@ -133,18 +141,32 @@ deletion_construct_displaytable <-
                                        "black", "black", "black", "#4EB0B5", "black", "#71519C", "#968BC2", "#DD76A5",
                                        "black", "black", "black", "black", "#D97F1D", "#71519C", "#968BC2", "#DD76A5",
                                        "black", "black", "black", "black", "black", "#71519C", "#968BC2", "#DD76A5")))
-  ))
-grid.arrange(deletion_construct_displaytable)
+  )) %>%
+  # the following code adds boxes aronud the table, row 1, and column 1
+  gtable_add_grob(
+    grobs = rectGrob(gp = gpar(fill = NA, lwd = 0.5)),
+    t = 1, b = 9, l = 1, r = 6) %>%
+  gtable_add_grob(
+    grobs = rectGrob(gp = gpar(fill = NA, lwd = 0.5)),
+    t = 1, b = 1, l = 1, r = 6) %>%
+  gtable_add_grob(
+    grobs = rectGrob(gp = gpar(fill = NA, lwd = 0.5)),
+    t = 1, b = 9, l = 1, r = 1)
 
-deletion_construct_plot <- wrap_elements(full = (wrap_elements(full = (ggdraw() + 
-                                                                          draw_image(image_read_svg(here("raw_data_analysis/figures/terminator_construct_designs/PIR1_motif_WT_construct_design.svg"))))) +
-                                                    wrap_elements(full = deletion_construct_displaytable) + 
-                                                    wrap_elements(full = (ggdraw() + 
-                                                                            draw_image(image_read_svg(here("raw_data_analysis/figures/terminator_construct_designs/tPIR1_design.svg"), width = 520, height = 354))))+
-                                                    plot_layout(design = deletion_layout))) /
-  PIR1_deltadeltacq_plot +
-  plot_annotation(tag_levels = "A") +
-  plot_layout(heights = c(3,1))
+## print the table to see how it looks
+grid.newpage()
+grid.draw(deletion_construct_displaytable)
+
+deletion_construct_plot <- 
+  plot_grid(NULL, # create top margin above ggdraw; probably there is a better way
+            ggdraw() + 
+              draw_image(image_read_svg(here("raw_data_analysis/figures/terminator_construct_designs/PIR1_motif_WT_construct_design.svg")) 
+              ),
+            deletion_construct_displaytable,
+            PIR1_deltadeltacq_plot,
+            rel_heights = c(0.02, 0.4, 0.7, 1),
+            ncol = 1,
+            labels = c("A","","B"))
 
 ggsave(here("results_chapter/figures/tPIR1_design_and_qpcr.png"),
        deletion_construct_plot,
